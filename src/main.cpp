10c7c0f4c5d01c425c477fc5ffb41de9a1ce0c9c
Diivanand Ramalingam <diivanand@gatech.edu>
2026-01-19 02:43:41 -0800

    fix compile issue

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,10 @@
 #include "glfw_initialization.h"
 #include "glfw_window.h"
-#include "vk_app.h"
 
 #include "../cmake-build-debug/_deps/glfw-src/include/GLFW/glfw3.h"
 
 int32_t main(int32_t, char**) {
     GlfwInitialization glfw; // RAII init/terminate
-    VkApp app;
-    app.run();
 
     std::unique_ptr<GLFWwindow, GLFWWindowDeleter> window = create_window(800, 600, "Vulkan Engine");
 
